af6df391b139878097b9eea8ab087aab03e8f2aa
Thilina Weerasinghe <Thilina1@users.noreply.github.com>
2022-07-01 17:08:37 +0530

    22/07/1

--- lib/sign_In/screens/user_info_screen.dart
+++ lib/sign_In/screens/user_info_screen.dart
@@ -7,6 +7,16 @@ import 'package:agrodoctor/sign_In/utils/authentication.dart';
 import 'package:agrodoctor/sign_In/widgets/app_bar_title.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+final List<String> imgList = [
+  "https://images.unsplash.com/photo-1560493676-04071c5f467b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+      "https://media.istockphoto.com/photos/shot-of-a-young-woman-using-a-digital-tablet-while-inspecting-crops-picture-id1365693929?s=612x612"
+      "https://media.istockphoto.com/photos/man-in-a-coffee-plantation-researcher-picture-id1338114224?s=612x612"
+      'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
 
 class UserInfoScreen extends StatefulWidget {
   const UserInfoScreen({Key? key, required User user})
@@ -294,7 +304,20 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
           child: Column(
             children: [
               const SizedBox(
-                height: 12,
+                height: 1,
+              ),
+              Container(
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    autoPlay: true,
+                    aspectRatio: 2.0,
+                    enlargeCenterPage: true,
+                  ),
+                  items: imageSliders,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
               ),
               Container(
                 child: Row(
@@ -309,6 +332,7 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
                             alignment: Alignment.topLeft,
                             child: Icon(
                               Icons.calculate_rounded,
+                              size: 30.0,
                             ),
                           ),
                           Text("Fertilizer Calculator")
@@ -328,6 +352,7 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
                             alignment: Alignment.topLeft,
                             child: Icon(
                               Icons.bug_report_rounded,
+                              size: 30.0,
                             ),
                           ),
                           Text("Pests & diseases"),
@@ -347,6 +372,7 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
                             alignment: Alignment.topLeft,
                             child: Icon(
                               Icons.compost_rounded,
+                              size: 30.0,
                             ),
                           ),
                           Text("Cultivation        Tips")
@@ -405,102 +431,7 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
                     borderRadius: BorderRadius.circular(10),
                     borderSide: BorderSide(color: Colors.white)),
               ),
-              _user.photoURL != null
-                  ? ClipOval(
-                      child: Material(
-                        color: CustomColors.firebaseGrey.withOpacity(0.3),
-                        child: Image.network(
-                          _user.photoURL!,
-                          fit: BoxFit.fitHeight,
-                        ),
-                      ),
-                    )
-                  : ClipOval(
-                      child: Material(
-                        color: CustomColors.firebaseGrey.withOpacity(0.3),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Icon(
-                            Icons.person,
-                            size: 60,
-                            color: CustomColors.firebaseGrey,
-                          ),
-                        ),
-                      ),
-                    ),
-              const SizedBox(height: 16.0),
-              Text(
-                'Hello',
-                style: TextStyle(
-                  color: CustomColors.firebaseGrey,
-                  fontSize: 26,
-                ),
-              ),
               const SizedBox(height: 8.0),
-              Text(
-                _user.displayName!,
-                style: TextStyle(
-                  color: CustomColors.firebaseYellow,
-                  fontSize: 26,
-                ),
-              ),
-              const SizedBox(height: 8.0),
-              Text(
-                '( ${_user.email!} )',
-                style: TextStyle(
-                  color: CustomColors.firebaseOrange,
-                  fontSize: 20,
-                  letterSpacing: 0.5,
-                ),
-              ),
-              const SizedBox(height: 24.0),
-              Text(
-                'You are now signed in using your Google account. To sign out of your account click the "Sign Out" button below.',
-                style: TextStyle(
-                    color: CustomColors.firebaseGrey.withOpacity(0.8),
-                    fontSize: 14,
-                    letterSpacing: 0.2),
-              ),
-              const SizedBox(height: 16.0),
-              _isSigningOut
-                  ? const CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                    )
-                  : ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                          Colors.redAccent,
-                        ),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                      onPressed: () async {
-                        setState(() {
-                          _isSigningOut = true;
-                        });
-                        await Authentication.signOut(context: context);
-                        setState(() {
-                          _isSigningOut = false;
-                        });
-                        Navigator.of(context)
-                            .pushReplacement(_routeToSignInScreen());
-                      },
-                      child: const Padding(
-                        padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
-                        child: Text(
-                          'Sign Out',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            letterSpacing: 2,
-                          ),
-                        ),
-                      ),
-                    ),
             ],
           ),
         ),
@@ -508,3 +439,36 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
     );
   }
 }
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          margin: EdgeInsets.all(5.0),
+          child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              child: Stack(
+                children: <Widget>[
+                  Image.network(item,
+                      fit: BoxFit.cover, width: double.infinity),
+                  Positioned(
+                    bottom: 0.0,
+                    left: 0.0,
+                    right: 0.0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromARGB(200, 0, 0, 0),
+                            Color.fromARGB(0, 0, 0, 0)
+                          ],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                    ),
+                  ),
+                ],
+              )),
+        ))
+    .toList();
